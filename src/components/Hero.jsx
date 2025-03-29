e80ba49cc88890d1bf66508ae6c7be4f07da8572
Saisandeep <sandeep.sangeetham845@gmail.com>
2025-03-29 11:27:55 +0530

    Added the Computer Canvas model Partially

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { styles } from '../styles.js';
+import { ComputersCanvas } from './canvas';
+import { TypeAnimation } from 'react-type-animation';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallbackCanvas from './canvas/ErrorFallbackCanvas.jsx';
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[130px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Saisandeep</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            <TypeAnimation sequence={[
+              'Full Stack Developer',
+              1000,
+              'Flutter Developer',
+              1000,
+              'ReactJs Developer',
+              1000,
+              'AI/ML Enthusiast',
+              1000,
+              'Problem Solver',
+              1000
+            ]}
+            wrapper='span'
+            cursor={true}
+            repeat={Infinity}
+            />
+          </p>
+        </div>
+      </div>
+
+      <ErrorBoundary FallbackComponent = {ErrorFallbackCanvas}
+        onReset={() =>{
+          window.location.reload();
+        }}
+      >      
+      <ComputersCanvas/>
+      </ErrorBoundary>
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Suspense, } from 'react';
+import { Canvas, extend } from '@react-three/fiber';
+import CanvasLoader from '../Loader';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+extend({ OrbitControls });
 
-const Computers = () => {
+const Model = () => {
+  const computer = useGLTF('/desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive 
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3, -1.5]}
+        rotation={[-0.01,-0.2,-0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* Need to add the fallback to the suspense as the canvas loader */}
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          autoRotate = {true}
+          autoRotateSpeed = {2}
+        />
+        <Model />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
